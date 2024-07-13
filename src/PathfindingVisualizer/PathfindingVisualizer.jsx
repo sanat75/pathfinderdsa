0c31dceeb06cd0059abdbf6e9aae5ed543d74721
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2024-07-14 03:49:59 +0530

    pushed

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -458,12 +458,6 @@ export default class PathfindingVisualizer extends Component {
             })}
           </tbody>
         </table>
-        {/* <button
-          type="button"
-          className="btn btn-danger"
-          onClick={() => this.clearGrid()}>
-          Clear Grid
-        </button> */}
         <button
           type="button"
           className="btn btn-warning"
